e1451c73ec679dcf222200c4b824dcbfda5b65e4
CodeNinja <gamerhud25@gmail.com>
2026-09-19 12:58:07 +0600

    fixed

--- common/src/main/java/net/montoyo/wd/miniserv/KeyParameters.java
+++ common/src/main/java/net/montoyo/wd/miniserv/KeyParameters.java
@@ -10,6 +10,6 @@ public abstract class KeyParameters {
     public static final int KEY_SIZE = 32;
     public static final int CHALLENGE_SIZE = 32;
     public static final String MAC_ALGORITHM = "HmacSHA256";
-    public static final String RSA_CIPHER = "RSA/ECB/PKCS1Padding";
+    public static final String RSA_CIPHER = "RSA/ECB/OAEPWithSHA-256AndMGF1Padding";
 
 }
